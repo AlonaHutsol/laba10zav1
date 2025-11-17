21fd1985fd7104833cb12e2bfa1e26365c307244
AlonaHutsol <gucolalona636@gmail.com>
2025-11-17 17:08:48 +0200

    Add files via upload

--- laba10zav1/laba10zav1/MyForm.h
+++ laba10zav1/laba10zav1/MyForm.h
@@ -1,4 +1,5 @@
 #pragma once
+				
 
 namespace laba10zav1 {
 
@@ -40,6 +41,14 @@ namespace laba10zav1 {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox6;
 
 	private:
 		/// <summary>
@@ -59,13 +68,21 @@ namespace laba10zav1 {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(208, 59);
+			this->textBox1->Location = System::Drawing::Point(208, 72);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(168, 37);
@@ -75,10 +92,10 @@ namespace laba10zav1 {
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox2->Location = System::Drawing::Point(208, 115);
+			this->textBox2->Location = System::Drawing::Point(208, 132);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(168, 80);
+			this->textBox2->Size = System::Drawing::Size(168, 36);
 			this->textBox2->TabIndex = 1;
 			// 
 			// label1
@@ -86,17 +103,18 @@ namespace laba10zav1 {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(43, 62);
+			this->label1->Location = System::Drawing::Point(43, 75);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(147, 20);
 			this->label1->TabIndex = 2;
-			this->label1->Text = L"Введіть 5 чисел";
+			this->label1->Text = L"Введіть 1 число";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(102, 231);
+			this->button1->Location = System::Drawing::Point(101, 398);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(110, 45);
 			this->button1->TabIndex = 3;
@@ -108,7 +126,7 @@ namespace laba10zav1 {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(256, 231);
+			this->button2->Location = System::Drawing::Point(378, 396);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(120, 48);
 			this->button2->TabIndex = 4;
@@ -116,11 +134,103 @@ namespace laba10zav1 {
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(43, 290);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(147, 20);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Введіть 5 число";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(43, 235);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(147, 20);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Введіть 4 число";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(43, 189);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(147, 20);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"Введіть 3 число";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(43, 135);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(147, 20);
+			this->label5->TabIndex = 8;
+			this->label5->Text = L"Введіть 2 число";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox3->Location = System::Drawing::Point(208, 186);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(168, 37);
+			this->textBox3->TabIndex = 9;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox4->Location = System::Drawing::Point(208, 232);
+			this->textBox4->Multiline = true;
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(168, 37);
+			this->textBox4->TabIndex = 10;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox5->Location = System::Drawing::Point(208, 287);
+			this->textBox5->Multiline = true;
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(168, 37);
+			this->textBox5->TabIndex = 11;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox6->Location = System::Drawing::Point(557, 135);
+			this->textBox6->Multiline = true;
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(193, 106);
+			this->textBox6->TabIndex = 12;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(415, 365);
+			this->ClientSize = System::Drawing::Size(906, 501);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label1);
@@ -137,47 +247,42 @@ namespace laba10zav1 {
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
 private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	// Отримуємо весь текст з textBox1
-	String^ input = textBox1->Text;
+	double a, b, c, d, f ;
 
-	// Розбиваємо на частини за пробілом
-	array<String^>^ parts = input->Split(' ');
+	// Перевіряємо правильність введення
+	if (!Double::TryParse(textBox1->Text, a)) { textBox6->Text = "Помилка у числі 1"; return; }
+	if (!Double::TryParse(textBox2->Text, b)) { textBox6->Text = "Помилка у числі 2"; return; }
+	if (!Double::TryParse(textBox3->Text, c)) { textBox6->Text = "Помилка у числі 3"; return; }
+	if (!Double::TryParse(textBox4->Text, d)) { textBox6->Text = "Помилка у числі 4"; return; }
+	if (!Double::TryParse(textBox5->Text, f)) { textBox6->Text = "Помилка у числі 5"; return; }
 
-	if (parts->Length != 5)
-	{
-		textBox2->Text = "Потрібно ввести рівно 5 чисел!";
-		return;
-	}
+	// Початкові мінімум і максимум — перше число
+	double minNum = a;
+	double maxNum = a;
 
-	// Перетворюємо перше число
-	double num;
-	if (!Double::TryParse(parts[0], num))
-	{
-		textBox2->Text = "Некоректне число!";
-		return;
-	}
+	// Перевіряємо всі числа
+	if (b < minNum) minNum = b;
+	if (c < minNum) minNum = c;
+	if (d < minNum) minNum = d;
+	if (f < minNum) minNum = f;
 
-	double minNum = num;
-	double maxNum = num;
+	if (b > maxNum) maxNum = b;
+	if (c > maxNum) maxNum = c;
+	if (d > maxNum) maxNum = d;
+	if (f > maxNum) maxNum = f;
 
-	// Обробляємо решту 4 чисел
-	for (int i = 1; i < 5; i++)
-	{
-		if (!Double::TryParse(parts[i], num))
-		{
-			textBox2->Text = "Некоректний формат числа!";
-			return;
-		}
-
-		if (num < minNum) minNum = num;
-		if (num > maxNum) maxNum = num;
-	}
-
-	textBox2->Text = "Мінімальне: " + minNum + "\nМаксимальне: " + maxNum;
+	// Вивід результату
+	textBox6->Text = "Мінімальне: " + minNum + "\r\nМаксимальне: " + maxNum;
 }
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	textBox1->Clear();
 	textBox2->Clear();	
+	textBox3->Clear();
+	textBox4->Clear();
+	textBox5->Clear();
+	textBox6->Clear();
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
